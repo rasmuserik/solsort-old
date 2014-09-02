dba80dba85f7b6d08e8548ae6588149e8d932c21
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2014-09-02 20:27:16 +0200

    reload server on codechange

--- src/solsort/server.cljs
+++ src/solsort/server.cljs
@@ -2,4 +2,10 @@
   (:require-macros [cljs.core.async.macros :refer [go alt!]])
   (:require [cljs.core.async :refer [>! <! chan put! take! timeout close!]]))
 
-(.log js/console "hello")
+(declare watcher)
+(defn reload []
+  (.close watcher)
+  (.reload js/location))
+(def watcher (.watch (js/require "fs") "build/server.js" reload))
+
+(set! (.-innerHTML js/document.body) "hello blah blah blah server dashboard")
