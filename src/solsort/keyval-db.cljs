d050a3ae4eb64b4dc44aa0f629b4475abdcb842a
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2014-12-13 09:48:29 +0100

    code for generating database

--- src/solsort/keyval-db.cljs
+++ src/solsort/keyval-db.cljs
@@ -1,38 +1,50 @@
 (ns solsort.keyval-db
   (:require-macros [cljs.core.async.macros :refer [go alt!]])
   (:require
+    [cljs.reader :refer [read-string]]
     [cljs.core.async :refer [>! <! chan put! take! timeout close!]]))
 
-
-
 (def stores (atom {}))
+
 (def db (atom nil))
 
 (defn open-db []
   (go
     (if @db (.close @db))
     ; BUG/WARNING  not multientrant
-    #_(let [c (chan)
-          req (.open js/indexedDB "keyval-db" (inc (count (keys stores))))])
-    ))
-
+    (let [c (chan)
+          store-list (read-string (.getItem js/localStorage "keyval-db"))
+          req (.open js/indexedDB "keyval-db" (inc (count store-list)))
+          ]
+      (set! (.-onupgradeneeded req)
+            #(let [db (.-result (.-target %))]
+               (doall (for [store store-list]
+                        (if (not (.contains (.-objectStoreNames db) store))
+                          (.createObjectStore db store))))))
+      (set! (.-onsuccess req) 
+            #(do 
+               (reset! db (.-result (.-target %)))
+               (close! c)))
+      (<! c))))
 (defn ensure-store [storage]
   (go
     (if (not (@stores storage)) 
       (do
         (swap! stores assoc storage {})
+        (.setItem js/localStorage "keyval-db" (str (keys @stores)))
         (<! (open-db))
         (print "storage" storage "missing")))
     (print (keys @stores))
     ))
-
-(defn multifetch [storage ids] (go #js{}))
-(defn fetch [storage id] (go (aget (<! (multifetch storage #js[id])) id)))
+(defn multifetch [storage ids] 
+  (go #js{}))
+(defn fetch [storage id] 
+  (go (aget (<! (multifetch storage #js[id])) id)))
 (defn store [storage id value] 
   (go
+    (<! (ensure-store storage))
     (swap! stores assoc storage (assoc (@stores storage) id value))
     (print stores)
-    (<! (ensure-store storage))
     ))
 (defn tryout []
   (go
@@ -48,67 +60,72 @@
 
 (def comment)
 (comment
-   (def db (atom nil))
-   (defn init [] 
-     (let [c (chan)]
-       (let [req (.open js/indexedDB "evildb" 1)]
-         (set! (.-onupgradeneeded req)
-               #(let [db (.-result (.-target %))]
-                  (if (.contains (.-objectStoreNames db) "keyvals")
-                    (.deleteObjectStore db "keyvals"))
-                  (.createObjectStore db "keyvals")))
-         (set! (.-onsuccess req) 
-               #(do 
-                  (reset! db (.-result (.-target %)))
-                  (close! c))))
-       c))
-
-   (def cache (atom #js{}))
-   (def cacheCount (atom 0))
-
-   (defn multifetch [storage ids]
-     (let [c (chan 1)]
-       (go
-         (<! (commit))
-         (let [result (atom #js{})
-               trans (.transaction @db #js["keyvals"] "readonly")
-               objStore (.objectStore trans "keyvals")]
-           (loop [i 0]
-             (if (< i (.-length ids))
-               (let [id (aget ids i)
-                     req (.get objStore (str storage ":" id))]
-                 (set! (.-onsuccess req) #(if (.-result req) (aset @result id (.-result req))))
-
-                 (recur (inc i)))))
-           (set! (.-oncomplete trans)  #(put! c @result)))
-         (<! c))))
 
 
-   (defn fetch [storage id] (go (aget (<! (multifetch storage #js[id])) id)))
 
-   (defn commit []
-     (let [c (chan 1)
-           trans (.transaction @db #js["keyvals"] "readwrite")
-           objStore (.objectStore trans "keyvals")]
-       (loop [ids (js/Object.keys @cache)]
-         (if (< 0 (.-length ids))
-           (let [id (.pop ids)
-                 req (.put objStore (aget @cache id) id)]
-             (set! (.-onerror trans)  
-                   (fn [e]
-                     (print e)
-                     (close! c)))
-             (recur ids))))
-       (reset! cacheCount 0)
-       (reset! cache #js{})
-       (set! (.-oncomplete trans)  #(put! c true))
-       (set! (.-onerror trans)  #(close! c))
-       c))
 
-   (defn store [storage id value]
-     (aset @cache (str storage ":" id) value)
-     (swap! cacheCount inc)
-     (if (< 1000 @cacheCount)
-       (commit)
-       (go)))
-   )
+
+  (def db (atom nil))
+  (defn init [] 
+    (let [c (chan)]
+      (let [req (.open js/indexedDB "evildb" 1)]
+        (set! (.-onupgradeneeded req)
+              #(let [db (.-result (.-target %))]
+                 (if (.contains (.-objectStoreNames db) "keyvals")
+                   (.deleteObjectStore db "keyvals"))
+                 (.createObjectStore db "keyvals")))
+        (set! (.-onsuccess req) 
+              #(do 
+                 (reset! db (.-result (.-target %)))
+                 (close! c))))
+      c))
+
+  (def cache (atom #js{}))
+  (def cacheCount (atom 0))
+
+  (defn multifetch [storage ids]
+    (let [c (chan 1)]
+      (go
+        (<! (commit))
+        (let [result (atom #js{})
+              trans (.transaction @db #js["keyvals"] "readonly")
+              objStore (.objectStore trans "keyvals")]
+          (loop [i 0]
+            (if (< i (.-length ids))
+              (let [id (aget ids i)
+                    req (.get objStore (str storage ":" id))]
+                (set! (.-onsuccess req) #(if (.-result req) (aset @result id (.-result req))))
+
+                (recur (inc i)))))
+          (set! (.-oncomplete trans)  #(put! c @result)))
+        (<! c))))
+
+
+  (defn fetch [storage id] (go (aget (<! (multifetch storage #js[id])) id)))
+
+  (defn commit []
+    (let [c (chan 1)
+          trans (.transaction @db #js["keyvals"] "readwrite")
+          objStore (.objectStore trans "keyvals")]
+      (loop [ids (js/Object.keys @cache)]
+        (if (< 0 (.-length ids))
+          (let [id (.pop ids)
+                req (.put objStore (aget @cache id) id)]
+            (set! (.-onerror trans)  
+                  (fn [e]
+                    (print e)
+                    (close! c)))
+            (recur ids))))
+      (reset! cacheCount 0)
+      (reset! cache #js{})
+      (set! (.-oncomplete trans)  #(put! c true))
+      (set! (.-onerror trans)  #(close! c))
+      c))
+
+  (defn store [storage id value]
+    (aset @cache (str storage ":" id) value)
+    (swap! cacheCount inc)
+    (if (< 1000 @cacheCount)
+      (commit)
+      (go)))
+  )
