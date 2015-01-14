4890bcfce567fe7a8e0f10abfd30d98f6f76b8aa
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2015-01-14 16:43:44 +0100

    bugfix

--- src/solsort/kvdb/leveldb.cljs
+++ src/solsort/kvdb/leveldb.cljs
@@ -6,12 +6,14 @@
 
 (def dbs (atom {}))
 (defn get-db [id]
-    (or (get @dbs id)
-        (get (reset! dbs (assoc @dbs id
-                ((js/require "levelup") 
-                 (str "./" (.replace (str id) #"[^a-zA-Z0-9]" "_") ".leveldb")
-                 #js{"valueEncoding" "json"}
-                 ))) id)))
+  (or (get @dbs id)
+      (get 
+        (reset! dbs (assoc @dbs id
+                           ((js/require "levelup") 
+                            (str "./" (.replace (str id) #"[^a-zA-Z0-9]" "_") ".leveldb")
+                            #js{"valueEncoding" "json"}))) 
+        id)))
+
 (defn commit [storage] (go))
 (defn fetch [storage id] 
   (let [c (chan 1)]
@@ -22,17 +24,21 @@
               (close! c)
               (put! c value))))
     c))
+
 (defn multifetch [storage ids]  
   (let [c (chan 1)
         result #js{}
         cnt (atom (count ids))]
-    (doall (for [id ids]
-             (take! (fetch storage id)
-                    (fn [value]
-                      (aset result id value)
-                      (if (<= (swap! cnt dec) 0)
-                        (put! c result))))))
+    (if (= 0 @cnt)
+      (close! c)
+      (doall (for [id ids]
+               (take! (fetch storage id)
+                      (fn [value]
+                        (aset result id value)
+                        (if (<= (swap! cnt dec) 0)
+                          (put! c result)))))))
     c))
+
 (defn store [storage id value]
   (let [c (chan 1)]
     (.put (get-db storage) 
