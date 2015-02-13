805b632284401611dbb848ccf1d7293017476e06
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2015-02-13 21:02:17 +0100

    cleanup

--- misc/start.sh
+++ misc/start.sh
@@ -1,2 +1,2 @@
 cd /home/server/solsort
-PATH=/home/server/local/bin:/usr/local/bin:$PATH forever start www/solsort.js server
+PATH=/home/server/local/bin:/usr/local/bin:$PATH forever start solsort.js server
