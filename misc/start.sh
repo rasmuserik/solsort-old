31f9efd999af3f2f12f9eb6f75b2b7ec7bef2401
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2015-02-19 14:49:00 +0100

    don't use forever

--- misc/start.sh
+++ misc/start.sh
@@ -1,2 +1,3 @@
 cd /home/server/solsort
-PATH=/home/server/local/bin:/usr/local/bin:$PATH forever start solsort.js server
+#PATH=/home/server/local/bin:/usr/local/bin:$PATH forever start solsort.js server
+PATH=/home/server/local/bin:/usr/local/bin:$PATH nohup ./dev-server.sh &
