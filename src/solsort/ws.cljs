c162abada3c2f41bcd40ccdd283a233092149d0b
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2015-02-19 21:48:22 +0100

    better websocket support

--- src/solsort/ws.cljs
+++ src/solsort/ws.cljs
@@ -3,50 +3,107 @@
     [cljs.core.async.macros :refer [go alt!]])
   (:require
     [solsort.registry :refer [testcase]]
-    [solsort.system :refer [is-nodejs is-browser log]]
+    [solsort.system :refer [is-nodejs is-browser log pid set-immediate]]
     [cljs.core.async :refer [>! <! chan put! take! timeout close!]]))
 
+
+(def incoming (chan))
+(def pids (atom {}))
+(declare send-message)
+(defn broadcast [msg]
+  (doall (for [pid (keys @pids)]
+           (send-message pid msg))))
+(defn send-message [pid msg] 
+  (.send (@pids pid) (js/JSON.stringify msg)))
+
+(comment server)
 (if is-nodejs
   (do
     (def ws (js/require "ws"))
+    (defn handle-message [pid] 
+      (fn [msg]
+        (let [msg (js/JSON.parse msg)]
+          (aset msg "src" (str "ws:" pid))
+          (put! incoming msg)
+          (log 'ws pid 'msg msg))))
+    (defn close-connection [id] 
+      (fn []
+        (log 'ws id 'close)
+        (swap! pids dissoc id)))
+    (defn start-websocket-server [http-server]
+      (log 'ws 'start)
+      (let [ws (js/require "ws")
+            wss (ws.Server. #js{:server http-server})]
+        (log 'ws ws wss)
+        (.on wss "connection" 
+             (fn [ws]
+               (log 'ws 'incoming-connection ws)
+               (console.log (js/Object.keys ws))
+               (.send ws (js/JSON.stringify  #js{:pid pid}))
+               (.on ws "message" 
+                    (fn [data flags]
+                      (let [data (js/JSON.parse data)
+                            pid (aget data "pid") ]
+                        (if pid
+                          (do
+                            (.removeAllListeners ws "message")
+                            (.on ws "message" (handle-message pid))
+                            (.on ws "close" (close-connection pid))
+                            (swap! pids assoc pid ws)
+                            (log 'ws 'added-client pid @pids)
+                            )
+                          (log 'ws 'error-unexpected-first-message data)
+                          ))))
+               ))
+        ))
     ))
 
-(defn start-websocket-server [http-server]
-  (log 'ws 'start)
-  (let [ws (js/require "ws")
-        wss (ws.Server. #js{:server http-server})]
-    (log 'ws ws wss)
-    (.on wss "connection" 
-         (fn [ws]
-           (log 'ws 'incoming-connection ws)
-           (.on ws "message" (fn [data flags]
-                               (.send ws data)
-                               (log 'ws 'receive data)))
-           (.on ws "close" (fn [data flags]
-                               (log 'ws 'close )))
-           (.send ws (js/JSON.stringify #js{ :hullo "hi"}))
-           ))
-    ))
 
+(comment browser)
 (if is-browser
   (do
     (def socket-server
-        (if (= -1 (.indexOf js/location.origin "localhost"))
-          "ws://ws.solsort.com/ws/"
-          "ws://localhost/ws/" 
-          ))
+      (if (= -1 (.indexOf js/location.origin "localhost"))
+        "ws://ws.solsort.com/ws/"
+        "ws://localhost/ws/" 
+        ))
 
-    (let
-      [ws (js/WebSocket. socket-server)]
-      (aset ws "onopen" (fn [e] 
-                          (log 'ws 'open e)
-                          (.send ws (js/JSON.stringify #js{:a "Hullo"}))
-                          ))
-      (aset ws "onerror" (fn [e] (log 'ws 'error) (js/console.log e)))
-      (aset ws "onclose" (fn [e] (log 'ws 'close e)))
-      (aset ws "onmessage" (fn [e] 
-                             (log 'ws 'message)
-                             (js/console.log e)
-                             ))
-      (log 'ws ws)
-      )))
+    (defn ws-connect []
+      (let
+        [ws (js/WebSocket. socket-server)]
+        (aset ws "onopen" (fn [e] (.send ws (js/JSON.stringify #js{:pid pid}))))
+        (aset ws "onerror" 
+              (fn [e] 
+                (log 'ws 'error) 
+                (js/console.log e)))
+        (aset ws "onclose" 
+              (fn [e] 
+                (log 'ws 'close e)
+                (set-immediate ws-connect)))
+        (aset ws "onmessage" 
+              (fn [e] 
+                (let [data (js/JSON.parse (aget e "data"))
+                      pid (aget data "pid")]
+                  (if pid
+                    (do
+                      (aset ws "onmessage" 
+                            (fn [e]
+                              (let [msg (js/JSON.parse (aget e "data"))]
+                                (aset msg "src" (str "ws:" pid))
+                                (put! incoming msg)
+                                (log 'ws pid 'msg msg))))
+                      (aset ws "onclose" 
+                            (fn [e] 
+                              (log 'ws 'close e pid)
+                              (swap! pids dissoc pid)
+                              (set-immediate ws-connect)))
+
+                      (swap! pids assoc pid ws)
+                      (log 'ws 'added-client pid @pids)
+                      )
+                    (log 'ws 'error-unexpected-first-message data)
+                    ))))
+        (log 'ws 'message)
+        ))
+    (ws-connect)
+    ))
