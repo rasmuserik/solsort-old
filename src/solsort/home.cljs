ce5c7918edb2b0bb17e31c2f1c96ead96204f260
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2015-02-22 10:04:53 +0100

    add state of project meta info in home-data

--- src/solsort/home.cljs
+++ src/solsort/home.cljs
@@ -129,6 +129,7 @@
          ))
 
 
+; state: unfinished|alpha|beta|done
 (add-entry 
   "Rasmus Erik Voel Jensen"
   ["developer" "company owner" "computer scientist"]
@@ -139,99 +140,99 @@
   "/visualisering-af-relationer/compare.html#relvis/cir870970-basis:05625351")
 (add-entry 
   "Visualisering af relationer"
-  ["2014" "production" "visualisering af relationer" "visualisation" "JavaScript"]
+  ["2014" "done" "visualisering af relationer" "visualisation" "JavaScript"]
   "https://vejlebib.dk/search/ting/musik#relvis/str870971-tsart:71029824,870971-tsart:71829375,870970-basis:49295642,870970-basis:07872992,870971-tsart:34418616,870970-basis:23454963,870970-basis:00117250,870971-tsart:73914493,870971-tsart:70501198,870971-tsart:70357151,870971-tsart:73443911,870970-basis:05385210,870970-basis:25722027,870970-basis:20269545,870970-basis:28902700,870970-basis:28799918,870971-tsart:33801262,870971-tsart:73950031,870970-basis:23292637,870970-basis:20826592,870970-basis:04971914,870970-basis:28799950,870970-basis:28799942,870970-basis:28205899,870970-basis:26386896,870970-basis:23702630,870970-basis:51445481,870970-basis:26747953,870971-tsart:87018148,870971-tsart:35714006i")
 (add-entry 
   "Sketch note draw"
-  ["2014" "experiment" "webapp" "infinite canvas" "zoomable"]
+  ["2014" "beta" "webapp" "infinite canvas" "zoomable"]
   "/sketch-note-draw/")
 (add-entry 
   "Frie Børnesange"
-  ["2014" "experiment" "webapp" "open content" "sangbog"]
+  ["2014" "alpha" "webapp" "open content" "sangbog"]
   "/frie-sange/")
 (add-entry 
   "Learn morse\xa0code"
-  ["2014" "experiment" "webapp"]
+  ["2014" "alpha" "webapp"]
   "/morse-code/")
 (add-entry 
   "Single touch snake"
-  ["2014" "experiment" "game" "webapp"]
+  ["2014" "unfinished" "game" "webapp"]
   "/single-touch-snake/")
 (add-entry 
   "Parkering i København"
-  ["2014" "hackathon" "open data day" "data.kk.dk" "gatesense" "iotpeople" "okfn"]
+  ["2014" "alpha" "hackathon" "open data day" "data.kk.dk" "gatesense" "iotpeople" "okfn"]
   "/kbh-parking/")
 (add-entry 
   "360º Viewer"
-  ["2014" "widget" "frontend" "hammertime"]
+  ["2014" "done" "widget" "frontend" "hammertime"]
   "/360/test.html")
 (add-entry 
   "Backend for UCC-organismen"
-  ["2014" "backend" "UCC Organismen" "ucc" "webuntis" "rejseplanen"]
+  ["2014" "done" "backend" "UCC Organismen" "ucc" "webuntis" "rejseplanen"]
   "http://ssl.solsort.com:8080/")
 (add-entry 
   "BibTekKonf Slides"
-  ["2013" "presentation" "dbc" "bibgraph"]
+  ["2013" "done" "presentation" "dbc" "bibgraph"]
   "/slides/bibtekkonf2013-bibgraph")
 (add-entry 
   "Art quiz"
-  ["2013" "prototype" "hack4dk"]
+  ["2013" "alpha" "prototype" "hack4dk"]
   "/hack4dk/quiz/")
 (add-entry 
   "Summer\xa0Hacks Slides"
-  ["2013" "copenhagenjs" "presentation" "bibgraph" "skolevej"]
+  ["2013" "done" "copenhagenjs" "presentation" "bibgraph" "skolevej"]
   "/slides/cphjs2013-summer-hacks")
 (add-entry 
   "BibGraph"
-  ["2013" "visualisation" "widget" "dbc" "adhl" "d3"]
+  ["2013" "alpha" "visualisation" "widget" "dbc" "adhl" "d3"]
   "http://labs.dbc.dk/bibgraph")
 (add-entry 
   "HTML5 Developer Perspective Slides"
-  ["2013" "presentation" "html5" "cnug"]
+  ["2013" "done" "presentation" "html5" "cnug"]
   "/slides/talk-html5-2013/cnug2013-slides.html")
 (add-entry 
   "Speeding visualisation"
-  ["2013" "visualisation" "hammertime" "role:optimisation" "role:reimplementation"]
+  ["2013" "done" "visualisation" "hammertime" "role:optimisation" "role:reimplementation"]
   "http://speeding.solsort.com/")
 (add-entry 
   "Dragimation"
-  ["2013" "widget" "hammertime" "legoland billund resort"]
+  ["2013" "done" "widget" "hammertime" "legoland billund resort"]
   "http://dragimation.solsort.com")
 (add-entry 
   "Pricing scale"
-  ["2013" "notes" "estimation tool"]
+  ["2013" "done" "notes" "estimation tool"]
   "/notes/pricing-scale")
 (add-entry 
   "Tsar Tnoc"
-  ["2012" "ludum dare" "hackathon"]
+  ["2012" "beta" "ludum dare" "hackathon"]
   "/tsartnoc/")
 (add-entry 
   "EuroCards"
-  ["2012" "card game"]
+  ["2012" "done" "card game"]
   "https://www.thegamecrafter.com/games/EuroCards")
 (add-entry 
   "BlobShot"
-  ["2012" "game" "hackathon" "5apps hackathon"]
+  ["2012" "alpha" "game" "hackathon" "5apps hackathon"]
   "/blobshot/")
 (add-entry 
   "CombiGame"
-  ["2012" "game" "hackathon"]
+  ["2012" "alpha" "game" "hackathon"]
   "http://old.solsort.com/#combigame")
 (add-entry 
   "Presentation evaluation notes"
-  ["2012" "notes" "toastmasters"]
+  ["2012" "done" "notes" "toastmasters"]
   "/notes/presentation-evaluation")
 (add-entry 
   "NoteScore"
-  ["2011" "app" "music" "edu"]
+  ["2011" "beta" "app" "music" "edu"]
   "https://play.google.com/store/apps/details?id=dk.solsort.notescore")
 (add-entry 
   "Danske Byer"
-  ["2011" "edu"]
+  ["2011" "alpha" "edu"]
   "http://solsort.com/danske-byer")
 (add-entry 
   "CuteEngine"
-  ["2011" "game" "unfinished"]
+  ["2011" "unfinished" "game" "unfinished"]
   "http://solsort.com/cute-engine")
 
 
