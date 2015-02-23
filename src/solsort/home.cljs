f7b4fce5151281f33ff626d4964c6aec9df6e664
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2015-02-23 11:03:44 +0100

    style+reformat

--- src/solsort/home.cljs
+++ src/solsort/home.cljs
@@ -111,149 +111,129 @@
   (js->react (clj->js o)))
 
 
-(defn show-html []
-  (print "HERE")
-  (if is-browser
-    (go
-      (log 'home (home-html))
-      ;(log 'home (js/React.renderToStaticMarkup (clj->react (form 1))))
-      (js/React.render (clj->react (home-html)) js/document.body)
-      ) ))
-
 (defn html-to-http [o]
   (if (:json-html o)
     #js{:http-headers #js{"Content-Type" "text/html;charset=UTF-8"}
-     :content
-     (str
-       "<!DOCTYPE html><html><head><title>"
-       (or (:title o) "solsort.com")
-       "</title><meta http-equiv=\"Content-Type\" content=\"text/html;charset=UTF-8\"><meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge,chrome=1\"><meta name=\"viewport\" content=\""
-       "width=device-width, initial-scale=1.0"
-       (if (:noscale o) ", minimum-scale=1.0, maximum-scale=1.0, user-scalable=0" "")
-       "\"><meta name=\"format-detection\" content=\"telephone=no\">"
-       (js/React.renderToStaticMarkup (js->react (:json-html o)))
-       "<script src=\"/react.min.js\"></script>"
-       "<script src=\"/solsort.js\"></script>"
-       "</head><body>"
-       "</body></html>")}))
+        :content
+        (str
+          "<!DOCTYPE html><html><head><title>"
+          (or (:title o) "solsort.com")
+          "</title><meta http-equiv=\"Content-Type\" content=\"text/html;charset=UTF-8\"><meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge,chrome=1\"><meta name=\"viewport\" content=\""
+          "width=device-width, initial-scale=1.0"
+          (if (:noscale o) ", minimum-scale=1.0, maximum-scale=1.0, user-scalable=0" "")
+          "\"><meta name=\"format-detection\" content=\"telephone=no\">"
+          "<style>
+          @font-face {
+          font-family: Ubuntu;
+          font-weight: 400;
+          src: url(/font/ubuntu-latin1.ttf) format(truetype);
+          }
 
+          body {
+          margin: 0px;
+          padding: 0px;
+          font-family: Ubuntu, sans-serif
+          }
+
+          div {
+          margin: 0;
+          padding: 0
+          }</style>"
+          "</head><body>"
+          (js/React.renderToStaticMarkup (js->react (:json-html o)))
+          "<script src=\"/react.min.js\"></script>"
+          "<script src=\"/solsort.js\"></script>"
+          "</body></html>")}))
 
 
 (route "index"
        (fn []
          (go
-         (log 'home 'index 'route is-browser)
-         (if is-browser
-           (show-html)
-           (html-to-http {:title "solsort.com" :json-html (clj->js (home-html))})
-           ))))
-
+           (log 'home 'index 'route is-browser)
+           (if is-browser
+             (js/React.render (clj->react (home-html)) js/document.body)
+             (html-to-http {:title "solsort.com" :json-html (clj->js (home-html))})
+             ))))
 
 ; state: unfinished|alpha|beta|done
-(add-entry 
-  "Rasmus Erik Voel Jensen"
-  ["developer" "company owner" "computer scientist"]
-  "/rasmuserik.html")
-(add-entry 
-  "Anbefalings-webservice"
-  ["2015" "beta" "visualisering af relationer" "webservice" "ClojureScript"]
-  "/visualisering-af-relationer/compare.html#relvis/cir870970-basis:05625351")
-(add-entry 
-  "Visualisering af relationer"
-  ["2014" "done" "visualisering af relationer" "visualisation" "JavaScript"]
-  "https://vejlebib.dk/search/ting/musik#relvis/str870971-tsart:71029824,870971-tsart:71829375,870970-basis:49295642,870970-basis:07872992,870971-tsart:34418616,870970-basis:23454963,870970-basis:00117250,870971-tsart:73914493,870971-tsart:70501198,870971-tsart:70357151,870971-tsart:73443911,870970-basis:05385210,870970-basis:25722027,870970-basis:20269545,870970-basis:28902700,870970-basis:28799918,870971-tsart:33801262,870971-tsart:73950031,870970-basis:23292637,870970-basis:20826592,870970-basis:04971914,870970-basis:28799950,870970-basis:28799942,870970-basis:28205899,870970-basis:26386896,870970-basis:23702630,870970-basis:51445481,870970-basis:26747953,870971-tsart:87018148,870971-tsart:35714006i")
-(add-entry 
-  "Sketch note draw"
-  ["2014" "beta" "webapp" "infinite canvas" "zoomable"]
-  "/sketch-note-draw/")
-(add-entry 
-  "Frie Børnesange"
-  ["2014" "alpha" "webapp" "open content" "sangbog"]
-  "/frie-sange/")
-(add-entry 
-  "Learn morse\xa0code"
-  ["2014" "alpha" "webapp"]
-  "/morse-code/")
-(add-entry 
-  "Single touch snake"
-  ["2014" "unfinished" "game" "webapp"]
-  "/single-touch-snake/")
-(add-entry 
-  "Parkering i København"
-  ["2014" "alpha" "hackathon" "open data day" "data.kk.dk" "gatesense" "iotpeople" "okfn"]
-  "/kbh-parking/")
-(add-entry 
-  "360º Viewer"
-  ["2014" "done" "widget" "frontend" "hammertime"]
-  "/360/test.html")
-(add-entry 
-  "Backend for UCC-organismen"
-  ["2014" "done" "backend" "UCC Organismen" "ucc" "webuntis" "rejseplanen"]
-  "http://ssl.solsort.com:8080/")
-(add-entry 
-  "BibTekKonf Slides"
-  ["2013" "done" "presentation" "dbc" "bibgraph"]
-  "/slides/bibtekkonf2013-bibgraph")
-(add-entry 
-  "Art quiz"
-  ["2013" "alpha" "prototype" "hack4dk"]
-  "/hack4dk/quiz/")
-(add-entry 
-  "Summer\xa0Hacks Slides"
-  ["2013" "done" "copenhagenjs" "presentation" "bibgraph" "skolevej"]
-  "/slides/cphjs2013-summer-hacks")
-(add-entry 
-  "BibGraph"
-  ["2013" "alpha" "visualisation" "widget" "dbc" "adhl" "d3"]
-  "http://labs.dbc.dk/bibgraph")
-(add-entry 
-  "HTML5 Developer Perspective Slides"
-  ["2013" "done" "presentation" "html5" "cnug"]
-  "/slides/talk-html5-2013/cnug2013-slides.html")
-(add-entry 
-  "Speeding visualisation"
-  ["2013" "done" "visualisation" "hammertime" "role:optimisation" "role:reimplementation"]
-  "http://speeding.solsort.com/")
-(add-entry 
-  "Dragimation"
-  ["2013" "done" "widget" "hammertime" "legoland billund resort"]
-  "http://dragimation.solsort.com")
-(add-entry 
-  "Pricing scale"
-  ["2013" "done" "notes" "estimation tool"]
-  "/notes/pricing-scale")
-(add-entry 
-  "Tsar Tnoc"
-  ["2012" "beta" "ludum dare" "hackathon"]
-  "/tsartnoc/")
-(add-entry 
-  "EuroCards"
-  ["2012" "done" "card game"]
-  "https://www.thegamecrafter.com/games/EuroCards")
-(add-entry 
-  "BlobShot"
-  ["2012" "alpha" "game" "hackathon" "5apps hackathon"]
-  "/blobshot/")
-(add-entry 
-  "CombiGame"
-  ["2012" "alpha" "game" "hackathon"]
-  "http://old.solsort.com/#combigame")
-(add-entry 
-  "Presentation evaluation notes"
-  ["2012" "done" "notes" "toastmasters"]
-  "/notes/presentation-evaluation")
-(add-entry 
-  "NoteScore"
-  ["2011" "beta" "app" "music" "edu"]
-  "https://play.google.com/store/apps/details?id=dk.solsort.notescore")
-(add-entry 
-  "Danske Byer"
-  ["2011" "alpha" "edu"]
-  "http://solsort.com/danske-byer")
-(add-entry 
-  "CuteEngine"
-  ["2011" "unfinished" "game" "unfinished"]
-  "http://solsort.com/cute-engine")
+(add-entry "Rasmus Erik Voel Jensen"
+           ["developer" "company owner" "computer scientist"]
+           "/rasmuserik.html")
+(add-entry "Anbefalings-webservice"
+           ["2015" "beta" "visualisering af relationer" "webservice" "ClojureScript"]
+           "/visualisering-af-relationer/compare.html#relvis/cir870970-basis:05625351")
+(add-entry "Visualisering af relationer"
+           ["2014" "done" "visualisering af relationer" "visualisation" "JavaScript"]
+           "https://vejlebib.dk/search/ting/musik#relvis/str870971-tsart:71029824,870971-tsart:71829375,870970-basis:49295642,870970-basis:07872992,870971-tsart:34418616,870970-basis:23454963,870970-basis:00117250,870971-tsart:73914493,870971-tsart:70501198,870971-tsart:70357151,870971-tsart:73443911,870970-basis:05385210,870970-basis:25722027,870970-basis:20269545,870970-basis:28902700,870970-basis:28799918,870971-tsart:33801262,870971-tsart:73950031,870970-basis:23292637,870970-basis:20826592,870970-basis:04971914,870970-basis:28799950,870970-basis:28799942,870970-basis:28205899,870970-basis:26386896,870970-basis:23702630,870970-basis:51445481,870970-basis:26747953,870971-tsart:87018148,870971-tsart:35714006i")
+(add-entry "Sketch note draw"
+           ["2014" "beta" "webapp" "infinite canvas" "zoomable"]
+           "/sketch-note-draw/")
+(add-entry "Frie Børnesange"
+           ["2014" "alpha" "webapp" "open content" "sangbog"]
+           "/frie-sange/")
+(add-entry "Learn morse\xa0code"
+           ["2014" "alpha" "webapp"]
+           "/morse-code/")
+(add-entry "Single touch snake"
+           ["2014" "unfinished" "game" "webapp"]
+           "/single-touch-snake/")
+(add-entry "Parkering i København"
+           ["2014" "alpha" "hackathon" "open data day" "data.kk.dk" "gatesense" "iotpeople" "okfn"]
+           "/kbh-parking/")
+(add-entry "360º Viewer"
+           ["2014" "done" "widget" "frontend" "hammertime"]
+           "/360/test.html")
+(add-entry "Backend for UCC-organismen"
+           ["2014" "done" "backend" "UCC Organismen" "ucc" "webuntis" "rejseplanen"]
+           "http://ssl.solsort.com:8080/")
+(add-entry "BibTekKonf Slides"
+           ["2013" "done" "presentation" "dbc" "bibgraph"]
+           "/slides/bibtekkonf2013-bibgraph")
+(add-entry "Art quiz"
+           ["2013" "alpha" "prototype" "hack4dk"]
+           "/hack4dk/quiz/")
+(add-entry "Summer\xa0Hacks Slides"
+           ["2013" "done" "copenhagenjs" "presentation" "bibgraph" "skolevej"]
+           "/slides/cphjs2013-summer-hacks")
+(add-entry "BibGraph"
+           ["2013" "alpha" "visualisation" "widget" "dbc" "adhl" "d3"]
+           "http://labs.dbc.dk/bibgraph")
+(add-entry "HTML5 Developer Perspective Slides"
+           ["2013" "done" "presentation" "html5" "cnug"]
+           "/slides/talk-html5-2013/cnug2013-slides.html")
+(add-entry "Speeding visualisation"
+           ["2013" "done" "visualisation" "hammertime" "role:optimisation" "role:reimplementation"]
+           "http://speeding.solsort.com/")
+(add-entry "Dragimation"
+           ["2013" "done" "widget" "hammertime" "legoland billund resort"]
+           "http://dragimation.solsort.com")
+(add-entry "Pricing scale"
+           ["2013" "done" "notes" "estimation tool"]
+           "/notes/pricing-scale")
+(add-entry "Tsar Tnoc"
+           ["2012" "beta" "ludum dare" "hackathon"]
+           "/tsartnoc/")
+(add-entry "EuroCards"
+           ["2012" "done" "card game"]
+           "https://www.thegamecrafter.com/games/EuroCards")
+(add-entry "BlobShot"
+           ["2012" "alpha" "game" "hackathon" "5apps hackathon"]
+           "/blobshot/")
+(add-entry "CombiGame"
+           ["2012" "alpha" "game" "hackathon"]
+           "http://old.solsort.com/#combigame")
+(add-entry "Presentation evaluation notes"
+           ["2012" "done" "notes" "toastmasters"]
+           "/notes/presentation-evaluation")
+(add-entry "NoteScore"
+           ["2011" "beta" "app" "music" "edu"]
+           "https://play.google.com/store/apps/details?id=dk.solsort.notescore")
+(add-entry "Danske Byer"
+           ["2011" "alpha" "edu"]
+           "http://solsort.com/danske-byer")
+(add-entry "CuteEngine"
+           ["2011" "unfinished" "game" "unfinished"]
+           "http://solsort.com/cute-engine")
 
 
 
