341a61488c4153f49031a9ca0a8e20ace5d607f6
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2015-02-24 11:56:23 +0100

    rasmuserik

--- src/solsort/rasmuserik.cljs
+++ src/solsort/rasmuserik.cljs
@@ -10,25 +10,31 @@
     [cljs.core.async :refer [>! <! chan put! take! timeout close! pipe]]))
 
 
-(def img-height 120)
+(def img-height 160)
 (def small-size 12)
 (def activity-height 130)
 (defn rasmuserik-html []
   [:div {:style {:text-align :center}}
-   [:div {:style {:font-size 16 :margin-top (/ img-height 2) :margin-bottom (/ img-height 2)}}
+   [:div {:style {
+                  :display :inline-block
+                  :max-width 720
+                  :text-align left
+                  :font-size 16 :margin-top (/ img-height 2) :margin-bottom (/ img-height 2)}}
     [:img {:src "/icons/rasmus-erik-voel-jensen"
            :style {:height img-height
                    :width img-height
                    :border-radius (/ img-height 2)
                    :vertical-align :middle
-                   :margin-right (/ img-height 4)
-                   :margin-left (/ img-height 4)
+                   :margin-right (/ img-height 8)
+                   :margin-left (/ img-height 8)
                    :box-shadow "0px 0px 2px #000"
                    }}]
     [:div {:style {:display :inline-block
                    :vertical-align :middle
                    :text-align :center
                    :margin 4
+                   :margin-right (/ img-height 8)
+                   :margin-left (/ img-height 8)
                    }}
 
      [:style (str "h1{font-weight:normal;margin-top:0}"
@@ -37,9 +43,7 @@
      [:div {:style {:font-size "100%"}}
       "Tingskrivervej\xa021\xa03tv \xa0 2400\xa0København\xa0NV \xa0 \xa0" [:br]
       "+45\xa060703081 \xa0 hej@solsort.com" 
-      ]]
-    [:div {:style {:display :inline-block :width (* img-height 1.5) :height 1}}]
-    ]
+      ]]]
    [:div 
     [:div {:style {:display :inline-block
                    :width 320
@@ -65,7 +69,7 @@
      [:h1 "Personal"]
      [:h2 "Current"]
      [:ul {:style {:min-height activity-height}}
-      [:li "Fatherhood - I am the father of a wonderful 2½ year old boy."]
+      [:li "Fatherhood - I am the father of a wonderful 2½ year old boy"]
       [:li "Dance - "
        [:a {:href "http://ci-cph.dk"} "Contact Impro"] ", "
        [:a {:href "http://www.swingshoes.dk/kalender-swingarrangementer/"} "Lindy Hop"] ", "
