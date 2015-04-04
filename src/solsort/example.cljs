fb7d7fdf8f6f1d56770ce9ad14683b8a92d94fbf
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2015-04-04 16:53:33 +0200

    code experiments

---
+++ src/solsort/example.cljs
@@ -0,0 +1,34 @@
+(ns solsort.example
+  (:require-macros [cljs.core.async.macros :refer [go go-loop alt!]])
+  (:require
+    [solsort.mbox :refer [route]]
+    [solsort.system :as system :refer [log is-browser fs source-file exit is-nodejs]]
+    [cljs.core.async :refer [>! <! chan put! take! timeout close! pipe]]))
+
+(defprotocol TestProtocol
+  (hello [this]))
+(extend-protocol TestProtocol
+  string
+  (hello [this] (str this "hullo" this)))
+
+(deftype Blah [x]
+  TestProtocol
+  (hello [this] (str this "huhu" x))
+  )
+(defrecord Blap [x]
+  TestProtocol
+  (hello [this] (str this "hihi" x))
+  )
+
+(route "hello" 
+       (fn [a b]
+        {:type "html"
+         :html [:div
+                "hello " 
+                (hello "world")
+                (hello (Blah. "foo"))
+                (hello (Blap. "foo"))
+                [:div (str "this is: " a)]
+                [:div [:a {:href "#hello/foo"} "foo"]]
+                [:div [:a {:href "#hello/bar"} "bar"]]
+                ]}))
