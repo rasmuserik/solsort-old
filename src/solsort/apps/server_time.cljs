df93d524ff38dd33fab86bd50dfa357964fa7eec
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2015-04-14 16:12:17 +0200

    server-time api

---
+++ src/solsort/apps/server_time.cljs
@@ -0,0 +1,9 @@
+(ns solsort.server-time
+  (:require-macros [cljs.core.async.macros :refer [go go-loop alt!]])
+  (:require
+    [solsort.sys.mbox :refer [route log]]
+    [solsort.sys.platform :refer [is-browser fs exit is-nodejs]]
+    [cljs.core.async :refer [>! <! chan put! take! timeout close! pipe]]))
+
+
+(route "server-time" (fn [] (.toISOString (js.Date.))))
