ad7b0b6e9fc3a8d16c700b87bad693cb3d77eaaf
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2015-04-14 12:11:52 +0200

    update server from webroot (remotely)

--- dev-server.sh
+++ dev-server.sh
@@ -9,8 +9,10 @@ echo $! > tail.pid
 
 while true
 do
-  node ./solsort.js dev-server &
+  node solsort.js dev-server &
   NODE_PID=$!
+  cp solsort.js ../webroot/solsort.js
   sleep 5
+  curl -s http://localhost:9999/index.html > build/webroot/index.html
   wait $NODE_PID
 done
---
+++ src/solsort/apps/update_server_from_webroot.cljs
@@ -0,0 +1,14 @@
+(ns solsort.apps.update-server-from-webroot
+  (:require-macros [cljs.core.async.macros :refer [go alt!]])
+  (:require
+    [solsort.sys.platform :refer [exec exit]]
+    [solsort.sys.mbox :refer [route log]]
+    [cljs.core.async :refer [>! <! chan put! take! timeout close!]]))
+
+(def shell-commands
+  (str
+    "cd ../webroot &&"
+    "git pull &&"
+    "cp solsort.js ../solsort/solsort.js"))
+
+(route "update-server-from-webroot" #(exec shell-commands))
