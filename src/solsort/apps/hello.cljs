a94098627be13cfa427ad1380e24cf5fec7dce0a
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2015-06-30 22:24:26 +0200

    we need to depend on all libraries for it to work, - log, dispatch, etc. has side-effects from getting loaded

--- src/solsort/apps/hello.cljs
+++ src/solsort/apps/hello.cljs
@@ -1,12 +1,22 @@
 (ns solsort.hello
-    (:require-macros
-           [cljs.core.async.macros :refer [go go-loop alt!]]
-          ; [solsort.example :refer [hello-swap]]
-           )
-    (:require
-           [solsort.sys.mbox :refer [route log]]
-           [solsort.sys.platform :refer [is-browser fs exit is-nodejs]]
-           [cljs.core.async :refer [>! <! chan put! take! timeout close! pipe]]))
+  (:require-macros
+    [cljs.core.async.macros :refer [go go-loop alt!]]
+    ; [solsort.example :refer [hello-swap]]
+    )
+  (:require
+    [solsort.lib.autorestart]
+    [solsort.lib.css]
+    [solsort.lib.dispatch]
+    [solsort.lib.html]
+    [solsort.lib.kvdb]
+    [solsort.lib.log]
+    [solsort.lib.net]
+    [solsort.lib.old-kvdb]
+    [solsort.lib.test-runner]
+    [solsort.lib.webserver]
+    [solsort.sys.mbox :refer [route log]]
+    [solsort.sys.platform :refer [is-browser fs exit is-nodejs]]
+    [cljs.core.async :refer [>! <! chan put! take! timeout close! pipe]]))
 
 (route
   "hello"
@@ -15,12 +25,12 @@
     {:type "html"
      :html
      [:form
-       [:input {:name "hello"}]
-       [:textarea {:name "here"}]
-       [:select {:name "hoo"}
-        [:option {:value "a"} "a"]
-        [:option {:value "n"} "n"]
-        [:option {:value "b"} "b"]]
-       [:input {:name "blah"}]]}))
+      [:input {:name "hello"}]
+      [:textarea {:name "here"}]
+      [:select {:name "hoo"}
+       [:option {:value "a"} "a"]
+       [:option {:value "n"} "n"]
+       [:option {:value "b"} "b"]]
+      [:input {:name "blah"}]]}))
 ;(events "hello" (fn [state kvs] [state kvs]))
 ;(render "hello" (fn [state] [:div "hello"]))
