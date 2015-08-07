852113805bdff5a1d53e3d1598700999b40e25d3
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2015-08-07 09:50:40 +0200

    update url-list, and do not change webroot

--- dev-server.sh
+++ dev-server.sh
@@ -12,15 +12,15 @@ while true
 do
   node solsort.js ${1:-dev-server} &
   NODE_PID=$!
-  cp solsort.js ../webroot/solsort.js
-  echo CACHE\ MANIFEST\ > ../webroot/solsort.appcache
-  echo "#" `date`  >> ../webroot/solsort.appcache
-  echo /solsort.js  >> ../webroot/solsort.appcache
-  echo /style.css >> ../webroot/solsort.appcache
-  echo /font/ubuntu-latin1.ttf >> ../webroot/solsort.appcache
-  echo NETWORK:  >> ../webroot/solsort.appcache
-  echo "*" >> ../webroot/solsort.appcache
-  sleep 5
-  curl -s http://localhost:9999/index.html > ../webroot/index.html
+#  cp solsort.js ../webroot/solsort.js
+#  echo CACHE\ MANIFEST\ > ../webroot/solsort.appcache
+#  echo "#" `date`  >> ../webroot/solsort.appcache
+#  echo /solsort.js  >> ../webroot/solsort.appcache
+#  echo /style.css >> ../webroot/solsort.appcache
+#  echo /font/ubuntu-latin1.ttf >> ../webroot/solsort.appcache
+#  echo NETWORK:  >> ../webroot/solsort.appcache
+#  echo "*" >> ../webroot/solsort.appcache
+#  sleep 5
+#  curl -s http://localhost:9999/index.html > ../webroot/index.html
   wait $NODE_PID
 done
