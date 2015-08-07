9071ea3013341c605458a3181126a70bbd968212
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2015-08-07 12:20:08 +0200

    server-running-script

---
+++ server.sh
@@ -0,0 +1,11 @@
+install -d logs
+npm install >& logs/`hostname`-`date +%y%m%d-%H%M%S`.install
+
+while true
+do
+  node solsort.js dev-server >& logs/`hostname`-`date +%y%m%d-%H%M%S`.console &
+  NODE_PID=$!
+  echo $NODE_PID > solsort.pid
+  sleep 5
+  wait $NODE_PID
+done
